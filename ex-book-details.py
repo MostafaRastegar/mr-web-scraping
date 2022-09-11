90c4410c63ce97b72a8de3bf8e21955dd591c9ba
m.rastegar <m.rastegar@fidibo.com>
2022-09-11 11:24:12 +0430

    Add book details example

--- ex-book-details.py
+++ ex-book-details.py
@@ -12,7 +12,7 @@ from helpers import hs, fg
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(
-    ChromeDriverManager().install())
+    ChromeDriverManager().install(), options=chrome_options)
 
 uri = "https://taaghche.com/book/30242/%D9%86%D8%A7%D8%B1%DA%AF%DB%8C%D9%84"
 
@@ -23,19 +23,24 @@ driver.get(uri)
 
 # print(' ================> create dataFrame')
 
-def getFirstSpan():
-    return fg.findElements(driver, "//a[contains(@class,'book_bookTitle')][0]/span[1]")
+def getChildFirstSpanText(nth):
+    return fg.findElement(
+        driver,
+        "//div[contains(@class,'moreInfo_moreInfoItems')]/div[contains(@class,'moreInfo_moreInfoItem')][" + str(nth) + "]/span[2]"
+    )
 
-print(fg.getText(getFirstSpan()))
 
-    
-# dict = {
-#     'pageTotals': fg.getText(fg.findElements(driver, "//a[contains(@class,'book_bookTitle')]")[0])
-#     'pageTotals': fg.getElements(driver, "//a[contains(@class,'book_bookTitle')]", fg.getText),
+print(getChildFirstSpanText(1))
 
-# }
+dict = {
+    'pageTotals': fg.getText(getChildFirstSpanText(1)),
+    'price': fg.getText(getChildFirstSpanText(2)),
+    'type': fg.getText(getChildFirstSpanText(3)),
+    'published': fg.getText(getChildFirstSpanText(4)),
+    'isbn': fg.getText(getChildFirstSpanText(5))
+}
 # print(' ================> fineshed')
 
-# df = pd.DataFrame(dict)
-# # saving the dataframe
-# df.to_csv('ex-book-details.csv')
+df = pd.DataFrame(dict, index=[0])
+# saving the dataframe
+df.to_csv('ex-book-details.csv')
